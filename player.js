ca894623a230881db400f2a02eddbad6c6ec36d3
Alexandre Tavares <xandy.poa@hotmail.com>
2025-06-22 15:08:54 -0300

    Ajuste nos botões. Adicionado as músicas e capa. Arquivo CSS criado.

--- player.js
+++ player.js
@@ -1,7 +1,22 @@
 const musicas = [
-  { nome: "Jazz Suave", arquivo: "musicas/jazz.mp3" },
-  { nome: "Rock Leve", arquivo: "musicas/rock.mp3" },
-  { nome: "Lo-fi Chill", arquivo: "musicas/lofi.mp3" }
+  {
+    nome: "Música Top",
+    artista: "Banda TOP",
+    arquivo: "musicas/music1.mp3",
+    capa: "capas/top.png"
+  },
+  {
+    nome: "Música Top 2",
+    artista: "Banda TOP",
+    arquivo: "musicas/music2.mp3",
+    capa: "capas/top.png"
+  },
+  {
+    nome: "Música Top 3",
+    artista: "Banda TOP",
+    arquivo: "musicas/music3.mp3",
+    capa: "capas/top.png"
+  }
 ];
 
 let indexAtual = 0;
@@ -11,16 +26,37 @@ const nomeMusica = document.getElementById("nome-musica");
 
 function carregarMusica(index) {
   audio.src = musicas[index].arquivo;
-  nomeMusica.textContent = "Música: " + musicas[index].nome;
-  audio.load();
+  document.getElementById("nome-musica").textContent = musicas[index].nome;
+  document.getElementById("artista").textContent = "Artista: " + musicas[index].artista;
+  document.getElementById("capa").src = musicas[index].capa;
+}
+
+function alternarPlayPause() {
+  const btn = document.getElementById("botao-play-pause");
+
+  if (audio.paused) {
+    audio.play();
+    btn.textContent = "⏸️";
+  } else {
+    audio.pause();
+    btn.textContent = "▶️";
+  }
 }
 
 function playMusica() {
   audio.play();
+  document.getElementById("botao-play-pause").textContent = "⏸️";
 }
 
 function pauseMusica() {
   audio.pause();
+  document.getElementById("botao-play-pause").textContent = "▶️";
+}
+
+function musicaAnterior() {
+  indexAtual = (indexAtual - 1 + musicas.length) % musicas.length;
+  carregarMusica(indexAtual);
+  playMusica();
 }
 
 function proximaMusica() {
